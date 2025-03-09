59d4f0b8eb2d0dcb146f7d5d32b53fd77fe73765
SeadogStudio <yort136@gmail.com>
2025-03-10 00:23:16 +0700

    Update BMDBLEController.h

--- src/BMDBLEController.h
+++ src/BMDBLEController.h
@@ -22,20 +22,33 @@ struct ParameterValue {
   bool valid;                // If parameter contains valid data
 };
 
+// Connection state enum
+enum BMDConnectionState {
+  BMD_STATE_DISCONNECTED,
+  BMD_STATE_SCANNING,
+  BMD_STATE_CONNECTING,
+  BMD_STATE_CONNECTED
+};
+
+// Forward declaration of callback classes
+class BMDAdvertisedDeviceCallbacks;
+class BMDSecurityCallbacks;
+
 class BMDBLEController {
 public:
   // Constructor & initialization
   BMDBLEController(String deviceName = "ESP32BMDControl");
+  ~BMDBLEController();
   void begin();
-  void loop();
   
-  // Connection methods
+  // Connection management
   bool scan(uint32_t duration = 10);
   bool connect();
   bool disconnect();
   bool isConnected();
   void clearBondingInfo();
   void setAutoReconnect(bool enabled);
+  BMDConnectionState getConnectionState();
   
   // Camera control methods
   bool setFocus(uint16_t rawValue);
@@ -43,72 +56,62 @@ public:
   bool setIris(float normalizedValue);
   bool setWhiteBalance(uint16_t kelvin);
   bool toggleRecording();
+  bool startRecording();
+  bool stopRecording();
   bool doAutoFocus();
   bool isRecording();
-  
-  // Raw command methods
-  bool sendRawCommand(uint8_t* commandData, size_t length);
-  bool sendFormattedCommand(uint8_t category, uint8_t parameter, 
-                           uint8_t dataType, uint8_t operation,
-                           uint8_t* data, size_t dataLength);
   
   // Parameter methods
   bool requestParameter(uint8_t category, uint8_t parameterId, uint8_t dataType);
   bool hasParameter(uint8_t category, uint8_t parameterId);
-  unsigned long getParameterTimestamp(uint8_t category, uint8_t parameterId);
-  
-  // Parameter access methods
-  String getParameterAsString(uint8_t category, uint8_t parameterId);
-  int32_t getParameterAsInt(uint8_t category, uint8_t parameterId);
-  float getParameterAsFloat(uint8_t category, uint8_t parameterId);
-  String getParameterAsHexString(uint8_t category, uint8_t parameterId);
-  bool getRawParameter(uint8_t category, uint8_t parameterId, uint8_t* buffer, size_t* bufferSize);
+  ParameterValue* getParameter(uint8_t category, uint8_t parameterId);
   
   // Event handling
-  void setResponseCallback(void (*callback)(uint8_t, uint8_t, String));
-  void setConnectionCallback(void (*callback)(bool));
+  typedef void (*ParameterCallback)(uint8_t category, uint8_t parameterId, uint8_t* data, size_t length);
+  typedef void (*ConnectionCallback)(BMDConnectionState state);
+  typedef void (*TimecodeCallback)(uint8_t hours, uint8_t minutes, uint8_t seconds, uint8_t frames);
+  typedef void (*PinRequestCallback)(uint32_t* pinCode);
   
-  // Public for testing/debugging (would normally be private)
-  bool _deviceFound;
-
+  void setParameterCallback(ParameterCallback callback);
+  void setConnectionCallback(ConnectionCallback callback);
+  void setTimecodeCallback(TimecodeCallback callback);
+  void setPinRequestCallback(PinRequestCallback callback);
+  
+  void loop(); // Must be called in Arduino loop()
+  
 private:
   // BLE connection handling
   bool setNotification(BLERemoteCharacteristic* pChar, bool enable, bool isIndication);
-  String extractTextData(uint8_t* pData, size_t length, int offset);
+  void checkConnection();
+  bool reconnect();
   
-  // Parameter storage
+  // Parameter storage and decoding
   ParameterValue* findParameter(uint8_t category, uint8_t parameterId);
   void storeParameter(uint8_t category, uint8_t parameterId, 
-                     uint8_t dataType, uint8_t operation, uint8_t* data, size_t dataLength);
-  String decodeParameterToString(ParameterValue* param);
+                      uint8_t dataType, uint8_t operation, uint8_t* data, size_t dataLength);
   
   // Static callbacks
-  static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
-                            uint8_t* pData, size_t length, bool isNotify, void* pThis) {
-    ((BMDBLEController*)pThis)->processIncomingPacket(pData, length);
-  }
+  static void controlNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
+                           uint8_t* pData, size_t length, bool isNotify);
   
   static void timecodeNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
-                                    uint8_t* pData, size_t length, bool isNotify, void* pThis) {
-    ((BMDBLEController*)pThis)->processTimecodePacket(pData, length);
-  }
+                            uint8_t* pData, size_t length, bool isNotify);
   
   static void statusNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
-                                  uint8_t* pData, size_t length, bool isNotify, void* pThis) {
-    ((BMDBLEController*)pThis)->processStatusPacket(pData, length);
-  }
+                          uint8_t* pData, size_t length, bool isNotify);
   
-  // Processing methods
+  // Packet processing
   void processIncomingPacket(uint8_t* pData, size_t length);
   void processTimecodePacket(uint8_t* pData, size_t length);
   void processStatusPacket(uint8_t* pData, size_t length);
   
   // Member variables
   String _deviceName;
-  bool _connected;
-  bool _scanInProgress;
+  BMDConnectionState _connectionState;
+  bool _deviceFound;
   bool _autoReconnect;
   bool _recordingState;
+  uint8_t _cameraStatus;
   
   BLEClient* _pClient;
   BLEAddress* _pServerAddress;
@@ -119,54 +122,38 @@ private:
   BLERemoteCharacteristic* _pCameraStatus;
   BLERemoteCharacteristic* _pDeviceName;
   
+  BMDAdvertisedDeviceCallbacks* _pScanCallbacks;
+  BMDSecurityCallbacks* _pSecurityCallbacks;
+  
   // Parameter storage
   #define MAX_PARAMETERS 64
   ParameterValue _parameters[MAX_PARAMETERS];
   int _paramCount;
   
-  // Timecode and status
-  String _timecodeStr;
-  uint8_t _cameraStatus;
+  // Timecode storage
+  uint8_t _timecodeHours;
+  uint8_t _timecodeMinutes;
+  uint8_t _timecodeSeconds;
+  uint8_t _timecodeFrames;
   
   // Timing variables
   unsigned long _lastReconnectAttempt;
   const unsigned long RECONNECT_INTERVAL = 5000; // 5 seconds
   
   // Callbacks
-  void (*_responseCallback)(uint8_t, uint8_t, String);
-  void (*_connectionCallback)(bool);
+  ParameterCallback _parameterCallback;
+  ConnectionCallback _connectionCallback;
+  TimecodeCallback _timecodeCallback;
+  PinRequestCallback _pinRequestCallback;
+  
+  static BMDBLEController* _instance; // Used for callbacks
   
   // Preferences for storing connection info
   Preferences _preferences;
   
-  // Security callback class
-  class BMDSecurityCallbacks : public BLESecurityCallbacks {
-  public:
-    BMDSecurityCallbacks(BMDBLEController* controller) : _controller(controller) {}
-    
-    uint32_t onPassKeyRequest();
-    void onPassKeyNotify(uint32_t pass_key) {}
-    bool onConfirmPIN(uint32_t pin) { return true; }
-    bool onSecurityRequest() { return true; }
-    void onAuthenticationComplete(esp_ble_auth_cmpl_t auth_cmpl);
-    
-  private:
-    BMDBLEController* _controller;
-  };
-  
-  // BLE scan callback class
-  class BMDAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  public:
-    BMDAdvertisedDeviceCallbacks(BMDBLEController* controller) : _controller(controller) {}
-    
-    void onResult(BLEAdvertisedDevice advertisedDevice);
-    
-  private:
-    BMDBLEController* _controller;
-  };
-  
-  BMDSecurityCallbacks* _pSecurityCallbacks;
-  BMDAdvertisedDeviceCallbacks* _pAdvertisedDeviceCallbacks;
+  // BLE callback classes
+  friend class BMDAdvertisedDeviceCallbacks;
+  friend class BMDSecurityCallbacks;
 };
 
 #endif // BMD_BLE_CONTROLLER_H
